0f83f87dfc6ffc1a1a1f0b58dac36dfdb61fd6bc
Marina Delas <marina.delas@gmail.com>
2019-12-05 22:36:14 +0100

    day2.2

--- 2/index.js
+++ 2/index.js
@@ -2,12 +2,11 @@ const getInput = require('../index');
 
 const input = getInput.getCommaSeparatedInput(2);
 
-input[1] = 12;
-input[2] = 2;
-
-const fn1 = array => {
-    let posOpcode = 0;
-    array.forEach((val, index) => {
+const fn1 = ([...array], noun, verb) => {
+  array[1] = noun;
+  array[2] = verb;
+  let posOpcode = 0;
+  array.forEach((val, index) => {
     if (index === posOpcode) {
       const opcode = val;
       if (opcode === 99) return;
@@ -15,13 +14,26 @@ const fn1 = array => {
         opcode === 1
           ? array[array[index + 1]] + array[array[index + 2]]
           : array[array[index + 1]] * array[array[index + 2]];
-        const posResult = array[index +3];
-        array[posResult] = result;
-        posOpcode += 4;
+      array[array[index + 3]] = result;
+      posOpcode += 4;
     }
   });
   return array;
 };
+// 6087827
+
+const fn2 = () => {
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      const result = fn1(input, noun, verb)[0];
+      if (result === 19690720) {
+        return [noun, verb];
+      }
+    }
+    verb = 0;
+  }
+};
+// [53, 79]
 
 module.exports = {
   fn1: fn1,
